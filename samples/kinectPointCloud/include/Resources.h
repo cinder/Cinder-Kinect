ac0a5af71b1824bd747d55babca77eceef58d059
Andrew Bell <cinder.afb@gmail.com>
2013-03-01 23:18:07 -0500

    Removed old kinectPointCloud sample

--- samples/kinectPointCloud/include/Resources.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "cinder/CinderResources.h"
-
-#define RES_VERT_ID				CINDER_RESOURCE( ../resources/, mainVert.glsl,	128, GLSL )
-#define RES_FRAG_ID				CINDER_RESOURCE( ../resources/, mainFrag.glsl,	129, GLSL )
