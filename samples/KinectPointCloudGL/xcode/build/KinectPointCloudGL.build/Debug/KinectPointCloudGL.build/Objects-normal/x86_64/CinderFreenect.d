c9dff377bddab906f039f622ccfe849f20bb89f6
Mike Allison <philosopher.osopher@gmail.com>
2015-04-28 22:56:06 -0400

    removed build and other xcode files

--- samples/KinectPointCloudGL/xcode/build/KinectPointCloudGL.build/Debug/KinectPointCloudGL.build/Objects-normal/x86_64/CinderFreenect.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/destructor/Documents/cinder_mike/blocks/Cinder-Kinect/src/CinderFreenect.cpp \
-  ../../../src/CinderFreenect.h ../../../src/freenect/libfreenect.h \
-  ../../../src/freenect/freenect_internal.h \
-  ../../../src/freenect/libfreenect-registration.h \
-  ../../../src/freenect/usb_libusb10.h ../../../src/freenect/libusb.h
